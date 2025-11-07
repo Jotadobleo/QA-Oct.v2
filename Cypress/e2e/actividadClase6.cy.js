51837693e4bfa408c94f1c0f6bbbde9ae7b96bcc
Jotadobleo <jotadobleo@gmail.com>
2025-11-07 03:33:33 -0300

    dolo2

---
+++ Cypress/e2e/actividadClase6.cy.js
@@ -0,0 +1,32 @@
+
+describe('Formulario de Registro', () => {
+  beforeEach(() => {
+    cy.visit('https://ticketazo.com.ar/auth/registerUser') 
+  })
+
+
+  it('Completa todos los campos y presiona Registrar (código base)', () => {
+    cy.registroDeUsuario()
+  })
+
+
+  it('Email ya registrado', () => {
+    cy.regConEmailRep()
+  })
+
+
+  it('Dni ya registrado', () => {
+    cy.regConDniRep()
+  })
+
+
+  it('Registro exitoso y listo para ingresar', () => {
+    cy.regRightPath()
+  })
+
+  it('Registro fallido por contraseña inválida', () => {
+    cy.regPassDudosa()
+    // Facu como me hizo renegar esta cosa (el git, el push, el merge y 37 errores)
+  })
+  
+})
